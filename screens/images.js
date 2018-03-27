dfc2df3834be262773aadd5bf94443347e2d8a9e
Alif <alif.numan@sphiere.com.bn>
2018-03-27 15:34:10 +0800

    Used React Native Elements Tile for displaying single image

--- screens/images.js
+++ screens/images.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { Platform, Text, TextInput, StyleSheet, View, Image, Alert, TouchableOpacity } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
-import { Header, Button} from 'react-native-elements'
+import { Header, Button, Tile} from 'react-native-elements'
 
 class ImageScreen extends Component{
     constructor(props) {
         super(props);
-        
     }
 
     state = {
         noOfLikes: ""
     };
     
-
     onLikePressHandler = () => {
         const { params } = this.props.navigation.state;
-        // const _id = params ? params._id : null;
         const images = params ? params.imageId : null;
         
-        // let imageUri = 'http://localhost:5000/api/images/';
         let imageUri = 'https://app-api-testing.herokuapp.com/api/images/';
         return fetch(imageUri + images, {
             method: 'POST',
@@ -27,32 +23,26 @@ class ImageScreen extends Component{
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
-
-        }).then((response) => response.json())
-            .then((responseJson) => {
-                console.log("[image js]responseJson:", responseJson)
-                // console.log("IMAGE LIKED:", this.imageLiked)
-                // noOfLikes = likes.length;
-            })                  
+		})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			console.log("[images js] responseJson:", responseJson)
+			// console.log("IMAGE LIKED:", this.imageLiked)
+			// noOfLikes = likes.length;
+		})
+		.catch(error => console.log('[images js] Error:', error));    
     }
 
     render(){
         const { params } = this.props.navigation.state;
-        console.log('PARAMS',params)
-        // const _id = params ? params._id : null;
+        console.log('[images js] PARAMS',params)
         const images = params ? params.imageId : null;
-        // const likes = params ? params.likes : null;
-        // console.log('LIKES: ', likes);
-        console.log('IMAGE ID: ', images);
-        
-        // noOfLikes = likes.length;
+        console.log('[images js] IMAGE ID: ', images);
 
         let imageUri = 'https://app-api-testing.herokuapp.com/api/images/';
-        // let imageUri = 'http://localhost:5000/api/images/';
-        console.log("Image selected", imageUri)
-        console.log("CURRENT IMAGE",images)
-        // console.log("IMAGE LIKED:", this.imageLiked)
-        // console.log("NO. OF LIKES:",responeseJson.likes)]
+        console.log("[images js] Image selected", imageUri)
+        console.log("[images js] CURRENT IMAGE",images)
+        
         var noOfLikes;
         fetch(imageUri + images, {
             method: 'GET',
@@ -60,50 +50,34 @@ class ImageScreen extends Component{
                 Accept: 'application/json',
                 'Content-Type': 'application/json'
             },
-        }).then((response) => response.json())
-            .then((responseJson) => {
-                console.log("[image js]Likes array:", responseJson)
-                let likes = responseJson.likes
-                console.log("NO OF LIKES: ", likes.length);
-                this.setState({noOfLikes: likes.length});
-            })
+		})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			console.log("[images js]Likes array:", responseJson)
+			let likes = responseJson.likes
+			console.log("[images js] NO OF LIKES: ", likes.length);
+			this.setState({noOfLikes: likes.length});
+		})
+		.catch(error => console.log('[images js] Error:', error));
 
         return (
-            // <View style={styles.imageContainer}>
-            //     <Header
-            //         leftComponent={{ icon: 'menu', color: '#fff' }}
-            //         centerComponent={{ text: "IMAGES", style: { color: "#fff" } }}
-            //         rightComponent={{ icon: 'home', color: '#fff' }}
-            //     />
+			
+            <View>
+				<Header centerComponent={{ text: "IMAGES", style: { color: "#fff" } }} />
 
-            //     <Image
-            //         source= {{uri: imageUri + images + '/display'}}
-            //         style={{height:'90%', width: '98%'}}
-            //     />
+				<Tile
+					imageSrc={{uri: imageUri + images + '/display'}}
+					title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolore exercitationem"
+					featured
+					caption="Some Caption Text"
+				/>
 
-            //     <Button
-            //         title= "Like" onPress={this.onLikePressHandler}
-            //     />
-            //     <Text>{this.state.noOfLikes}</Text> 
-            // </View>
-
-            <View style={{
-                flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-            }}>
-                <Header
-                    leftComponent={{ icon: 'menu', color: '#fff' }}
-                    centerComponent={{ text: "IMAGES", style: { color: "#fff" } }}
-                    rightComponent={{ icon: 'home', color: '#fff' }}
-                />
-
-                <Image
+                {/* <Image
                     source= {{uri: imageUri + images + '/display'}}
                     style={{height:'80%', width: '100%'}}
                     alignItems='center'
                     justifyContent='center'
-                />
+                /> */}
 
                 <Button
                     title= "Like" onPress={this.onLikePressHandler}
@@ -111,27 +85,28 @@ class ImageScreen extends Component{
                 <View style= {styles.textContainer}>
                     <Text >{this.state.noOfLikes}</Text> 
                 </View>
-                
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+	outerContainer: {
+		flex:1,
+		flexDirection: 'column',
+		justifyContent: 'space-between'
+	},
     imageContainer: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
         height: '100%',
-        width: '100%',
-
+		width: '100%',
     },
-
     textContainer: {
         alignItems: 'center',
         justifyContent: 'center'
     }
-
 })
 module.exports = ImageScreen;
