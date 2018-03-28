49ece6f3feeae2fe228a36699baf18adcb77e6fa
Alif <alif.numan@sphiere.com.bn>
2018-03-28 11:56:17 +0800

    Removed unneeded codes

--- screens/images.js
+++ screens/images.js
@@ -7,17 +7,20 @@ class ImageScreen extends Component{
     constructor(props) {
 		super(props);
 		//TODO: check if props.navigation.state.params exists
-		console.log('[images js] constructor: ', props.navigation.state.params);
+		console.log('[images js] constructor - passedParams: ', props.navigation.state.params);
 
 		// check if image liked
 		let isLiked = false;
-		props.navigation.state.params.data.likes.forEach(function(likeId){
-			console.log('[images js] constructor - likeId: ', likeId);
-			if(likeId == props.navigation.state.params.userId){
-				console.log('[images js] constructor - User already liked this image.');
-				isLiked = true;
-			}
-		});
+		if (typeof props.navigation.state.params.data.likes !== 'undefined' && 
+		props.navigation.state.params.data.likes.length > 0){
+			props.navigation.state.params.data.likes.forEach(function(likeId){
+				console.log('[images js] constructor - likeId: ', likeId);
+				if(likeId == props.navigation.state.params.userId){
+					console.log('[images js] constructor - User already liked this image.');
+					isLiked = true;
+				}
+			});
+		}
 
 		// initialize state
 		this.state = {
@@ -28,6 +31,7 @@ class ImageScreen extends Component{
 			noOfLikes: props.navigation.state.params.data.likes.length
 		};
 
+		// logs
 		console.log('[images js] constructor - After init.');
 		console.log('[images js] constructor - imageId: ', this.state.imageId);
 		console.log('[images js] constructor - imageLikesArray: ', this.state.imageLikesArray);
@@ -35,28 +39,13 @@ class ImageScreen extends Component{
 		console.log('[images js] constructor - isImageLiked: ', this.state.isImageLiked);
 		console.log('[images js] constructor - noOfLikes: ', this.state.noOfLikes);
     }
-	
-	fetchImageDetails = (imageUri, imageId) => {
-        return fetch(imageUri + imageId, {
-            method: 'GET',
-            headers:{
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-		})
-		.then((response) => response.json())
-		.then((responseJson) => {
-			console.log("[images js] Likes array:", responseJson);
-			console.log("[images js] NO OF LIKES: ", responseJson.likes.length);
-		})
-		.catch(error => console.log('[images js] Error:', error));
-	}
     
     onLikePressHandler = (imageUri,imageId) => {
-		console.log('[images js] onLikePressHandler Like btn Pressed!');
-		console.log('[images js] onLikePressHandler imageUri: ', imageUri);
-		console.log('[images js] onLikePressHandler imageId: ', imageId);
-        
+		console.log('[images js] onLikePressHandler - Like btn Pressed!');
+		console.log('[images js] onLikePressHandler - imageUri: ', imageUri);
+		console.log('[images js] onLikePressHandler - imageId: ', imageId);
+		
+		// send request to API
         return fetch(imageUri + imageId, {
             method: 'POST',
             headers: {
@@ -66,12 +55,12 @@ class ImageScreen extends Component{
 		})
 		.then((response) => response.json())
 		.then((responseJson) => {
-			console.log("[images js] onLikePressHandler responseJson:", responseJson);
+			console.log("[images js] onLikePressHandler - responseJson:", responseJson);
 
 			// check if image liked
 			let isLiked = false;
 			const userId = this.state.userId;
-			console.log("[images js] onLikePressHandler responseJson Likes: ", responseJson.likes);
+			console.log("[images js] onLikePressHandler - responseJson Likes: ", responseJson.likes);
 			if (typeof responseJson.likes !== 'undefined' && responseJson.likes.length > 0) {
 				responseJson.likes.forEach(function(likeId){
 					console.log('[images js] onLikePressHandler - likeId: ', likeId);
@@ -87,45 +76,15 @@ class ImageScreen extends Component{
 				isImageLiked: isLiked
 			});
 		})
-		.catch(error => console.log('[images js] onLikePressHandler Error:', error));    
+		.catch(error => console.log('[images js] onLikePressHandler - Error:', error));    
 	}
 
     render(){
-        // const { params } = this.props.navigation.state;
-		// console.log('[images js] PARAMS PASSED FROM USER JS: ',params);
-		
-        // const imageId = params ? params.data._id : "";
-		// console.log('[images js] IMAGE ID: ', imageId);
-
-		// const imageLikesArray = params ? params.data.likes : "";
-		// console.log('[images js] IMAGE LIKES Array: ', imageLikesArray);
-		
-		// const currentUserId = params ? params.userId : "";
-		// console.log('[images js] Currently logged in user id: ', params.userId);
-
-		// let isLiked = false;
-		// imageLikesArray.forEach(function(likeId){
-		// 	console.log('[images js] likeId: ', likeId);
-		// 	if(likeId == currentUserId){
-		// 		console.log('[images js] User already liked this image.');
-		// 		isLiked = true;
-		// 	}
-		// });
-		
-		// get likes id
-		// if likes id matches with current user id
-			// set btn icon to full
-		// else
-			// set btn icon to empty
-		
-        let imageUri = 'https://app-api-testing.herokuapp.com/api/images/';
-        console.log("[images js] Image path: ", imageUri);
-        console.log("[images js] CURRENT IMAGE ID: ",this.state.imageId);
-        
-		// this.fetchImageDetails(imageUri, imageId);
+        const imageUri = 'https://app-api-testing.herokuapp.com/api/images/';
+        console.log("[images js] render - Image path: ", imageUri);
+        console.log("[images js] render - CURRENT IMAGE ID: ",this.state.imageId);
 		
         return (
-			
             <View>
 				<Header centerComponent={{ text: "IMAGES", style: { color: "#fff" } }} />
 
@@ -133,10 +92,6 @@ class ImageScreen extends Component{
 					imageSrc={{uri: imageUri + this.state.imageId + '/display'}}
 					title="Random titletext"
 				/>
-
-                {/* <Button
-                    title= "Like" onPress={this.onLikePressHandler}
-				/> */}
 				
                 <View style= {styles.textContainer}>
                     <Text >{this.state.noOfLikes}</Text> 
@@ -152,10 +107,8 @@ class ImageScreen extends Component{
 						color='#f50'
 					/>
 				</TouchableOpacity>
-
-					
             </View>
-        )
+        );
     }
 }
 
