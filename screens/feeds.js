f15736c018d0b13bcb2efad9f781cf27308a7620
Alif <alif.numan@sphiere.com.bn>
2018-04-06 15:57:33 +0800

    Combined user and followed images

--- screens/feeds.js
+++ screens/feeds.js
@@ -34,7 +34,9 @@ class FeedsScreen extends Component {
              followedImagesContainer: [],
 			 followIDArray: [],
 			 oneFollowMultiImageFlag: false,
-			 feedImagesArray: []
+			 feedImagesArray: [],
+			 areUserImagesLoading: true,
+			 areFollowedUsersImagesLoading: true,
         }
         
         //check for number of follows and who
@@ -43,9 +45,23 @@ class FeedsScreen extends Component {
 	}
 
 	componentDidMount(){
+		let userImagesArray = this.getUserImages();
+		console.log('[feeds js] componentDidMount - userImagesArray: ', userImagesArray);
+
+		// let followedImagesArray = this.getFollowedUsersImages();
+		// console.log('[feeds js] componentDidMount - followedImagesArray: ', followedImagesArray);
+
+		// if(!this.state.areFollowedUsersImagesLoading)
+		// 	this.setState({ feedImagesArray: [...userImagesArray, ...followedImagesArray] });
+
 		console.log('[feeds js] componentDidMount - Number of following: ', this.state.followed.length);
 		let tempFeedImagesArray = [];
-		if(this.state.followed.length === 1){
+		if(this.state.followed.length === 0){
+			// 0 FOLLOWED
+			this.setState({
+				feedImagesArray: [...userImagesArray, ...tempFeedImagesArray]
+			});
+		}else if(this.state.followed.length === 1){
 			// ONE FOLLOWED
 			console.log('[feeds js] componentDidMount - One followed: ', this.state.followed);
 			console.log('[feeds js] componentDidMount - One followed Contents: ', this.state.followed[0]);
@@ -76,7 +92,7 @@ class FeedsScreen extends Component {
 					});
 
 					this.setState({
-						feedImagesArray: [...tempFeedImagesArray]
+						feedImagesArray: [...userImagesArray , ...tempFeedImagesArray]
 					});
 				}
 			})
@@ -109,30 +125,11 @@ class FeedsScreen extends Component {
 				// ]
 				console.log('[feeds js] componentDidMount - Multiple followed response: ', response);
 				
-				// let tempFeedImagesArray = response.data.map((item, index) => {
-				// 	if(item.images.length >= 1){
-				// 		item.images.forEach((imageId,index) => {
-				// 			let tempImageUri = GET_IMAGES_URI + imageId + '/display';
-				// 			return (
-				// 				<Button 
-				// 					transparent style={styles.thumbnail} 
-				// 					onPress={() => this.onImageClicked(imageId, this.state.passedId)} 
-				// 					key={imageId} 
-				// 				>
-				// 					<Thumbnail
-				// 						large square source={{ uri: tempImageUri }}
-				// 					/>
-				// 				</Button>
-				// 			);
-				// 		});	
-				// 	}		
-				// });
 				// loop through followed users
-				let tempArray1 = [];
 				response.data.forEach((followedUser,index) => {
 					// check image array length
 					if(followedUser.images.length >= 1){
-						let tempArray2 = followedUser.images.map((imageId,index) => {
+						let tempFeedImagesArray2 = followedUser.images.map((imageId,index) => {
 							let tempImageUri = GET_IMAGES_URI + imageId + '/display';
 							return(
 								<Button 
@@ -146,180 +143,48 @@ class FeedsScreen extends Component {
 								</Button>
 							);
 						});
-						tempArray1 = [...tempArray1, ...tempArray2];
+						tempFeedImagesArray = [...userImagesArray, ...tempFeedImagesArray, ...tempFeedImagesArray2];
 					}
 				});
 				this.setState({
-					feedImagesArray: [...tempArray1]
+					feedImagesArray: [...tempFeedImagesArray]
 				});
 				console.log('[feeds js] componentDidMount - Multiple feedImagesArray: ', this.state.feedImagesArray);
 			})
 			.catch(error => console.log('[feeds js] componentDidMount - MULTIPLE followed fetch error: ', error));
 		}
-		// if(this.state.followed.length == 1){
-		// 	// ONE followed
-		// 	console.log('[feeds js] componentDidMount - One followed: ', this.state.followed);
-		// 	console.log('[feeds js] componentDidMount - One followed Contents: ', this.state.followed[0]);
-		// 	const followedId = this.state.followed[0];
+	}
 
-		// 	return fetch(GET_USERS_URI+followedId)
-		// 		.then(response => response.json())
-		// 		.then(response => {
-		// 			console.log('[feeds js] componentDidMount - fetch response: ', response);
-		// 			console.log('[feeds js] componentDidMount - fetch response images: ', response.images);
-		// 			console.log('[feeds js] componentDidMount - fetch response images length: ', response.images.length);
+	getUserImages(){
+		let userImagesArray = [];
+		// let imageThumbnail;
+		//check if user have images and display them
+		if(typeof this.state.images != undefined && this.state.images != null && this.state.images.length != null && this.state.images.length > 0){
 
-		// 			if(response.images.length == 0){
-		// 				// zero images
-		// 				console.log('[feeds js] componentDidMount - fetch response images length: ZERO');
-		// 				let tempImageHeading = [( <Text key={response.username}>{response.username}</Text> )];
-		// 				let tempImageElement = [( <Text key={followingId}>No images available.</Text> )];
-		// 				this.setState({ 
-		// 					followImageHeading: [...tempImageHeading],
-		// 					followedImagesContainer: [...tempImageElement] 
-		// 				});
-		// 			}else if(response.images.length == 1){
-		// 				// one image
-		// 				console.log('[feeds js] componentDidMount - fetch response images length: ONE');
-		// 				const tempImageUri = GET_IMAGES_URI + response.images[0] + '/display';
-		// 				let tempImageHeading = [( <Text key={response.username}>{response.username}</Text> )];
-		// 				let tempImageElement = [( 
-		// 					// <TouchableOpacity
-		// 					// 	key={response.images[0]}
-		// 					// 	style={styles.thumbnail}
-		// 					// 	onPress={() => this.onImageClicked2(response.images[0], this.state.passedId)}
-		// 					// >
-		// 					// 	<Image
-		// 					// 		key={response.images[0]}
-		// 					// 		source={{uri:tempImageUri}}
-		// 					// 		style={styles.thumbnail}
-		// 					// 	/>
-		// 					// </TouchableOpacity>
-		// 					<Button
-		// 						transparent style={styles.thumbnail} 
-		// 						onPress={() => this.onImageClicked(response.images[0], this.state.passedId)} 
-		// 						key={response.images[0]} >
-		// 						<Thumbnail
-		// 							key={response.images[0]}
-		// 							large square source={{ uri: tempImageUri}}
-		// 						/>
-		// 					</Button>
-		// 				)];
-		// 				this.setState({ 
-		// 					followImageHeading: [...tempImageHeading],
-		// 					followedImagesContainer: [...tempImageElement] 
-		// 				});
-		// 			}else if(response.images.length > 1){
-		// 				// multiple images
-		// 				console.log('[feeds js] componentDidMount - fetch response images length: MORE THAN ONE');
-		// 				// map
-		// 				let tempImageElement = response.images.map((imageId, index) => {
-		// 					let tempImageUri = GET_IMAGES_URI + imageId + '/display';
-		// 					return (
-		// 						// <TouchableOpacity
-		// 						// 	key={imageId + index}
-		// 						// 	style={styles.thumbnail}
-		// 						// 	onPress={() => this.onImageClicked2(imageId, this.state.passedId)}
-		// 						// >
-		// 						// 	<Image
-		// 						// 		key={imageId}
-		// 						// 		source={{uri:tempImageUri}}
-		// 						// 		style={styles.thumbnail}
-		// 						// 	/>
-		// 						// </TouchableOpacity>
-		// 						<Button 
-		// 							transparent style={styles.thumbnail} 
-		// 							onPress={() => this.onImageClicked(imageId, this.state.passedId)} 
-		// 							key={imageId} 
-		// 						>
-		// 							<Thumbnail
-		// 								large square source={{ uri: tempImageUri }}
-		// 							/>
-		// 						</Button>
-		// 					);
-		// 				});
-		// 				this.setState({
-		// 					oneFollowMultiImageFlag: true, 
-		// 					followedImagesContainer: [...tempImageElement] 
-		// 				});
-		// 			}
-		// 		})
-		// 		.catch(error => console.log('[feeds js] componentDidMount - ONE followed fetch error: ', error));
+			// find out length of image array
+			console.log("[feeds js] getUserImages Image Count: ", this.state.images.length);
 
-		// }else if(this.state.followed.length > 1){
-		// 	// MULTIPLE followed
-		// 	console.log('[feeds js] componentDidMount - Multiple followed: ', this.state.followed);
-		// 	return fetch(GET_USERS_FOLLOWED_URI)
-		// 		.then(response => response.json())
-		// 		.then(response => {
-		// 			console.log('[feeds js] componentDidMount - Multiple followed fetch response: ', response);
-
-		// 			let tempImageElements = [];
-		// 			// modify and then save response data to tempImagesArray
-		// 			let tempImagesArray = response.data.map((item, index) => {
-		// 				console.log('[feeds js] componentDidMount - MULTIPLE fetch map: ', item);
-		// 				console.log('[feeds js] componentDidMount - MULTIPLE fetch image arrays: ', item.images);
-		// 				console.log('[feeds js] componentDidMount - MULTIPLE fetch image arrays length: ', item.images.length);
-	
-		// 				if(item.images.length <= 0){
-		// 					// TODO: logic for when followed user does not have images
-		// 				}else if(item.images.length >= 1){
-		// 					// followed user has multiple images
-		// 					// modify and then save each image to tempImageElements
-		// 					let tempImageElements = item.images.map((item,index) => {
-		// 						let tempImageUri =GET_IMAGES_URI + item + '/display';
-		// 						return (
-		// 							// <TouchableOpacity
-		// 							// 	key={item + index}
-		// 							// 	style={styles.thumbnail}
-		// 							// 	onPress={() => this.onImageClicked2(item, this.state.passedId)}
-		// 							// >
-		// 							// 	<Image
-		// 							// 		key={item}
-		// 							// 		source={{ uri: tempImageUri }}
-		// 							// 		style={styles.thumbnail}
-		// 							// 	/>
-		// 							// </TouchableOpacity>
-		// 							<Button 
-		// 								transparent style={styles.thumbnail}
-		// 								key={item} 
-		// 								onPress={() => this.onImageClicked(item, this.state.passedId)} 
-		// 							>
-		// 								<Thumbnail
-		// 									key={item}
-		// 									large square source={{ uri: tempImageUri }}
-		// 								/>
-										
-		// 							</Button>
-		// 						);
-		// 					});
-
-		// 					console.log('[feeds js] componentDidMount - MULTIPLE fetch tempImageElements: ', tempImageElements);
-		// 					// return View element containing tempImageElements 
-		// 					return (
-		// 						<View style={styles.followedImagesOuter}
-		// 							key = {item._id + index}
-		// 						>
-		// 							<Text>{item.username}</Text>
-		// 							<View style={styles.followedImagesInner}>
-		// 								{[...tempImageElements]}
-		// 							</View>
-									
-		// 						</View>
-		// 					);
-		// 				}
-		// 			});
-		// 			this.setState({
-		// 				followedImagesContainer: [...tempImagesArray]
-		// 			});
-		// 		})
-		// 		.catch(error => console.log('[feeds js] componentDidMount - Multiple followed fetch error: ', error));
-		// }
+			if(this.state.images.length >= 1 ){
+				userImagesArray = this.state.images.map((imageId, index) => {
+					return (
+						<Button 
+							transparent style={styles.thumbnail} 
+							onPress={() => this.onImageClicked(imageId, this.state.passedId)} 
+							key={imageId} >
+							<Thumbnail 
+							large square  source={{ uri: GET_IMAGES_URI + imageId + '/display' }} 
+							/> 
+						</Button>
+					); 
+				});
+			}
+		}
+		console.log("[feeds js] getUserImages userImagesArray: ", userImagesArray);
+		return userImagesArray;
 	}
 	
 	onLogoutHandler = () => {
         return fetch('https://app-api-testing.herokuapp.com/logout', {
-        // return fetch('http://localhost:5000/logout', {
             method: 'GET',
             headers: {
                 Accept: 'application/json',
@@ -474,164 +339,50 @@ class FeedsScreen extends Component {
 	}
 
 	render() {
-        // console.log('[user js] render PICTURES: ', this.state.images);
-        console.log('[feeds js] followedImagesContainer:', this.state.followedImagesContainer);
-       
-        let imageElement;
-        let imageThumbnail;
-        //check if user have images and display them
-        if(typeof this.state.images != undefined && this.state.images != null && this.state.images.length != null && this.state.images.length > 0){
-           console.log('[feeds js] render IN THE IF STATEMENT');
-           console.log('[feeds js] render IF STATEMENT',(this.state.images != undefined || this.state.images.length == 0)); 
-           // find out length of image array
-            let imageCount = this.state.images.length;
-		    console.log("[feeds js] render Image Count: ", imageCount);
-		    // setup variable to contain Image element
-            // let imageUri = 'https://app-api-testing.herokuapp.com/api/images/';
-            // let imageUri = 'http://localhost:5000/api/images/';
-            //if user has only one image
-		    if(imageCount == 1){
-                console.log("[feeds js] render IMAGE ID", this.state.images);
-                console.log("[feeds js] render IMAGE URI", GET_IMAGES_URI + this.state.images[0])
-			    imageElement = (
-                // <TouchableOpacity
-                //     onPress={() => this.onImageClicked(this.state.images[0], this.state.passedId )}
-                //     style={styles.thumbnail}    
-                // >
-                //     <Image 
-				// 	    source={{uri: GET_IMAGES_URI + this.state.images[0] + '/display'}}
-                //         style={styles.thumbnail} 
-                //     />
-                // </TouchableOpacity>
-					<Button 
-						transparent style={styles.thumbnail} 
-						onPress={() => this.onImageClicked(this.state.images[0], this.state.passedId)} 
-					>
-						<Thumbnail
-								large square  source={{ uri: GET_IMAGES_URI + this.state.images[0] + '/display' }}
-						/> 
-					</Button>
-					
-                
-                );
-                
-                
-		    }else if(imageCount > 1 ){
-			    imageElement = [];
-			    imageElement = this.state.images.map((imageId, index) => {
-				    return (
-					    // <TouchableOpacity
-                        //     onPress={() => this.onImageClicked(imageId, this.state.passedId)}
-                        //     key={imageId} 
-                        //     style={styles.thumbnail}
-                        // >
-
-                        //     <Image  
-                        //         source={{ uri: GET_IMAGES_URI + imageId + '/display'}}
-                        //         style={styles.thumbnail} 
-                        //     />
-						// </TouchableOpacity>
-						<Button 
-							transparent style={styles.thumbnail} 
-							onPress={() => this.onImageClicked(imageId, this.state.passedId)} 
-							key={imageId} 
-						>
-							<Thumbnail 
-									large square  source={{ uri: GET_IMAGES_URI + imageId + '/display' }} 
-							/> 
-						</Button>
-                    );
-                    
-			    });
-
-		    }
-        }else {
-            // console.log("[user js] render IN THE ELSE STATEMENT");
-            imageElement = (
-				<Text>No images available.</Text>
-			);
-        }
-		
         return (
-        //     <View style={styles.page}>
-		// 		<Header
-		// 			centerComponent={{ text: this.state.passedUsername, style: { color: "#fff" } }}
-		// 			rightComponent={<CustomLogoutBtn clicked={this.onLogoutHandler} />}
-		// 			outerContainerStyles={styles.outerHeader}
-		// 		/>
-
-		// 		<View style={styles.body}>
-
-		// 			<View style={styles.imagesContainer}>
-
-		// 				<View style={styles.currentUserOuter}>
-		// 					<Text>{this.state.passedUsername}</Text>
-		// 					<View style={styles.currentUserInner}>
-		// 						{imageElement}
-		// 					</View>
-		// 				</View>
-
-		// 				<View style={ this.state.oneFollowMultiImageFlag ? styles.followedUsersOuterSingle : styles.followedUsersOuterMulti}>
-		// 					{this.state.followedImagesContainer}
-		// 				</View>
-
-		// 			</View>
-
-		// 			<View style={styles.buttonsContainer}>
-		// 				<Button title="Image Picker" onPress={this.onImagePickerHandler} />
-		// 				<Button title="Explore" onPress={()=>{this.onExplorePressedHandler(this.state.passedId)}} />
-		// 				<Text>{this.state.log}</Text>
-		// 			</View>
-
-		// 		</View>
-        //     </View>
-        
-        <Container>
-            <Header>
-				<Left>
-					
-				</Left>
-                <Body><Title>{this.state.passedUsername}</Title></Body>
-				<Right>
-					<Button transparent onPress={this.onLogoutHandler}>
-						<Icon name='home' />
-					</Button>
-				</Right>
-            </Header>
-            <Content>
-				<Grid>
-					<Text>USER IMAGES</Text>
-					<Row>
-						<View style={styles.imagesContainer}>
-							{imageElement}
-						</View>
-					</Row>
-					<Text>FOLLOWED IMAGES</Text>
-					<Row>
+			<Container>
+				<Header>
+					<Left>
+						
+					</Left>
+					<Body><Title>{this.state.passedUsername}</Title></Body>
+					<Right>
+						<Button transparent onPress={this.onLogoutHandler}>
+							<Icon name='home' />
+						</Button>
+					</Right>
+				</Header>
+				<Content>
+					{/* <Grid>
+						<Row style={{justifyContent:'center'}}>
+							<View style={styles.imagesContainer}>
+								{this.state.feedImagesArray}
+							</View>
+						</Row>
+						<Row>
+							<Text>{this.state.log}</Text>
+						</Row>
+					</Grid> */}
+					<View>
 						<View style={styles.imagesContainer}>
 							{this.state.feedImagesArray}
 						</View>
-					</Row>
-					<Row>
-						<Text>{this.state.log}</Text>
-					</Row>
-				</Grid>
-                
-            </Content>
-			<Footer>
-				<FooterTab >
-					<Button full onPress={this.onImagePickerHandler}>
-						<Text>Image Picker</Text>
-					</Button>
-					<Button full onPress={() => { this.onExplorePressedHandler(this.state.passedId) }}>
-						<Text>Explore</Text>
-					</Button>
-					<Button full onPress={() => { this.onProfilePressedHandler() }}>
-						<Text>Profile</Text>
-					</Button>
-				</FooterTab>
-			</Footer>
-        </Container>
+					</View>
+				</Content>
+				<Footer>
+					<FooterTab >
+						<Button full onPress={this.onImagePickerHandler}>
+							<Text>Image Picker</Text>
+						</Button>
+						<Button full onPress={() => { this.onExplorePressedHandler(this.state.passedId) }}>
+							<Text>Explore</Text>
+						</Button>
+						<Button full onPress={() => { this.onProfilePressedHandler() }}>
+							<Text>Profile</Text>
+						</Button>
+					</FooterTab>
+				</Footer>
+			</Container>
         );
     }
 }
